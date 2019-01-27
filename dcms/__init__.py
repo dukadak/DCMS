d7dba2534d277c82216bd95ab7e6cb9e1c604b1b
sonkadak <jhy070997@naver.com>
2019-01-27 15:53:25 +0900

    add asset pages

--- dcms/__init__.py
+++ dcms/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from dcms.views import view
 
 app = Flask(__name__)
+app.register_blueprint(view.page)
 
 @app.errorhandler(404)
 def page_not_found(error):
---
+++ dcms/views/view.py
@@ -0,0 +1,13 @@
+from flask import Flask, Blueprint, render_template, url_for
+
+page = Blueprint('page', __name__, template_folder='templates')
+
+@page.route('/assets')
+def show():
+    return render_template('assets.html')
+
+@page.route('/add_assets')
+def add_assets():
+    return render_template('add_assets.html')
+
+    
